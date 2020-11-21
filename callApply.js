4900fcb7e82185cc7261d1ab7f163a4fb619d285
mhgitrepo <mehedi1921@gmail.com>
2020-11-22 01:36:42 +0600

    object-mastering-interview-questions

---
+++ callApply.js
@@ -0,0 +1,19 @@
+const newObject= {
+    firstName: "James",
+    lastName: "William",
+    salary: 10000,
+    getFullName: function(){
+        console.log(this.firstName, this.lastName);
+    },
+    bill: function(amount){
+        this.salary = this.salary - amount;
+        return this.salary;
+    }
+}
+
+// console.log(newObject.firstName);
+newObject.bill(1000);
+newObject.bill(2000);
+// newObject.bill(500);
+// console.log(newObject.bill(500));
+console.log(newObject.salary);
